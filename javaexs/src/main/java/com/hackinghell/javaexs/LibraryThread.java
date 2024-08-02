4dc2d0e9e69a92baeb2f5784509cef3704c88e52
DuarteVLoureiro <duartevloureiro@gmail.com>
2024-08-02 14:19:16 +0100

    LibraryThread

---
+++ javaexs/src/main/java/com/hackinghell/javaexs/LibraryThread.java
@@ -0,0 +1,8 @@
+package com.hackinghell.javaexs;
+
+public class LibraryThread implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("im getting signs from library");
+    }
+}
